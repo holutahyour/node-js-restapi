1a53364409d60e0fac57bcd806b85657df688713
Victor Onibudo <tonibudo@lagetronix.com>
2022-06-08 23:09:51 +0100

    initialize

---
+++ src/config/config.ts
@@ -0,0 +1,22 @@
+export const config = {
+  "dev": {
+    "username": "tahyour",
+    "password": "Jesusluv01",
+    "database": "udagramtahyourdev",
+    "host": "udagramtahyourdev.cwczntmpvhyy.us-east-1.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-1",
+    "aws_profile": "default",
+    "aws_media_bucket": ""
+  },
+  "jwt": {
+    "secret": " "
+  },
+  "prod": { 
+    "username": "",
+    "password": "",
+    "database": "",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
